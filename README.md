9e553d19b0fee44aab8fd557c4ce90b04f73ac62
Todd Tate <toddtatevw@gmail.com>
2019-03-28 08:42:53 -0600

    What I have now along with Readme file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# AJAX_Site
-Giphy Ajax Website
+# Giftastic!
+A project I completed during the Univeersity Of Denver Full Stack Coding Bootcamp. This site uses the power of the Giphy API. Search terms are appended to the page and results are returned from Giphy. Enjoy!
+## Getting Started
+Enter or Click a search term to get started.
+## Built With
+* [AJAX] (https://www.w3schools.com/xml/ajax_intro.asp) - AJAX
+* [Jquery](https://jquery.com/) - JQuery
+* [Javascript](https://www.javascript.com/) - Javascript
+* [HTML] (https://html.com/) - HTML
+* [CSS] (https://www.w3schools.com/css/) - CSS
+## Author
+* **Todd Tate** - *Initial work* - [ToddTateDev](https://github.com/ToddTateDev)
+## Acknowledgments
+* Google-Fu
--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
 var searchArray = ["trump", "dogs", "cats", "hamburger", "birds", "willsmith", "comedy", "wtf", "cars", "food"];
 
-function displaySearchTerm () {
+function displaySearchTerm() {
     var searchTerm = $(this).attr("data-searchterm");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=yiqC8G0guS0zsGnjeIao5QfVBGDA7GQ7&limit=10";
-
+    console.log("Function Working");
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         var results = response.data;
-        var searchDiv = $("<div class='search'>");
-        var rating = results.rating;
-        var ratingParag = $("<p>").text("Rating: " + rating);
-        searchDiv.append(ratingParag);
-        console.log(response);
-        var image = results.images.fixed_width.url;
-        var imageTag = $("<img>").attr("src", image);
-        searchDiv.append(imageTag);
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i]);
+            var searchDiv = $("<div class='search'>");
+            var rating = results[i].rating;
+            var ratingParag = $("<p>").text("Rating: " + rating);
+            searchDiv.append(ratingParag);
+            
+            var image = results[i].images.fixed_width.url;
+            var imageTag = $("<img>").attr("src", image);
+            searchDiv.append(imageTag);
 
-        $("#gifs-appear-here").prepend(searchDiv);
+            $("#gifs-appear-here").prepend(searchDiv);
+
+        };
     })
 };
 
-function renderButtons () {
+function renderButtons() {
     $("#buttons").empty();
     for (var i = 0; i < searchArray.length; i++) {
-        var createButton = $("<button type='button' class='btn btn-secondary'>");
+        var createButton = $("<button type='button' class='btn btn-secondary gif-button'>");
         createButton.attr("data-searchterm", searchArray[i]);
         createButton.text(searchArray[i]);
         $("#buttons").append(createButton);
     }
 }
-    
-$("#add-search").on("click", function(event) {
+
+$("#add-search").on("click", function (event) {
     event.preventDefault();
     var search = $("#search-input").val().trim();
     searchArray.push(search);
@@ -40,7 +44,7 @@ $("#add-search").on("click", function(event) {
     renderButtons();
 })
 
-$(document).on("click", ".btn btn-secondary", displaySearchTerm);
+$(document).on("click", ".gif-button", displaySearchTerm);
 renderButtons();
 
 
